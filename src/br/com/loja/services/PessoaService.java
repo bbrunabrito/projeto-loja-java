2dc4ab20bdc201f159e7b746ab3ce8517ac97006
bbrunabrito <brunabritoalves@hotmail.com>
2024-01-22 10:12:39 -0300

    Atualizações no PessoaService: melhora no código

--- src/br/com/loja/services/PessoaService.java
+++ src/br/com/loja/services/PessoaService.java
@@ -13,53 +13,22 @@ public class PessoaService {
     private static List<Cliente> listaClientes = new ArrayList<>();
     private static List<Vendedor> listaVendedores = new ArrayList<>();
 
-    private static void salvaCliente(Cliente novoCliente) throws VendaException {
-        if (listaClientes.contains(novoCliente)) {
-            throw new VendaException("Erro no cadastro do Cliente", "O cliente já foi registrado no sistema", CodigoErro.CLIENTE_REGISTRADO_ERRO);
-        } else {
-            listaClientes.add(novoCliente);
-            System.out.println("Cliente registrado com sucesso!");
-            System.out.println("\n*****************************\n");
-        }
-    }
-
-    private static void salvaVendedor(Vendedor novoVendedor) throws VendaException {
-            if (listaVendedores.contains(novoVendedor)) {
-                throw new VendaException("Erro no cadastro do vendedor", "O vendedor já está registrado no sistema", CodigoErro.VENDEDOR_REGISTRADO_ERRO);
-            } else {
-                listaVendedores.add(novoVendedor);
-                System.out.println("Vendedor registrado com sucesso!");
-                System.out.println("\n*****************************\n");
-            }
-        }
-
     public static void registraPessoa(Pessoa pessoa) {
-        if (pessoa instanceof Cliente) {
-            try {
+        try {
+            if (pessoa instanceof Cliente) {
                 salvaCliente((Cliente) pessoa);
-            }
-            catch (VendaException e) {
-                System.out.println(e);
-            }
-            finally {
                 imprimeListaClientes();
-            }
-        } else {
-            try {
+            } else {
                 salvaVendedor((Vendedor) pessoa);
-            }
-            catch (VendaException e) {
-                System.out.println(e);
-            }
-            finally {
                 imprimeListaVendedores();
             }
+        } catch (VendaException e) {
+            System.out.println(e);
         }
     }
 
     public static void imprimeListaVendedores() {
-        System.out.println("-------------------");
-        System.out.println("Lista de Vendedores Registrados: ");
+
         for (Vendedor vendedor : listaVendedores) {
             System.out.print(vendedor.toString());
             System.out.println("\n-------------------\n");
@@ -75,4 +44,26 @@ public class PessoaService {
         }
         System.out.println("-------------------");
     }
+
+    private static void salvaCliente(Cliente novoCliente) throws VendaException {
+        if (listaClientes.contains(novoCliente)) {
+            throw new VendaException("Erro no cadastro do Cliente", "O cliente já foi registrado no sistema", CodigoErro.CLIENTE_REGISTRADO_ERRO);
+        } else {
+            listaClientes.add(novoCliente);
+            System.out.println("Cliente registrado com sucesso!");
+            System.out.println("\n*****************************\n");
+        }
+    }
+
+    private static void salvaVendedor(Vendedor novoVendedor) throws VendaException {
+        if (listaVendedores.contains(novoVendedor)) {
+            throw new VendaException("Erro no cadastro do vendedor", "O vendedor já está registrado no sistema", CodigoErro.VENDEDOR_REGISTRADO_ERRO);
+        } else {
+            listaVendedores.add(novoVendedor);
+            System.out.println("Vendedor registrado com sucesso!");
+            System.out.println("\n*****************************\n");
+        }
+    }
+
+
 }
