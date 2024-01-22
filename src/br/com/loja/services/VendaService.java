723b76698052cd57f925b7f85ddbde486d115279
bbrunabrito <brunabritoalves@hotmail.com>
2024-01-22 10:15:27 -0300

    Atualizações no VendaService: melhora na nomenclatura dos métodos

--- src/br/com/loja/services/VendaService.java
+++ src/br/com/loja/services/VendaService.java
@@ -14,17 +14,51 @@ public class VendaService {
     private static List<Venda> listaVendas = new ArrayList<>();
     private static List<Venda> carrinho = new ArrayList<>();
 
-    public static void registraVenda(Venda item) {
-        if (possivelComprar(item.getProduto(), item.getQuantidade())) {
-            item.setValorVenda(item.getProduto().getPreco().multiply(item.getQuantidade()));
-            item.setDataVenda(LocalDateTime.now());
-            listaVendas.add(item);
-            ProdutoService.subtraiEstoque(item.getProduto(), item.getQuantidade());
 
-            System.out.println("\n" +  "Venda realizada com sucesso!" +
-                    "\nValor da venda: " + item.getValorVenda() +
-                    "\nProduto: " + item.getProduto().getDescricao());
+    public static void processaCompra() {
+        BigDecimal valorTotalVenda = BigDecimal.ZERO;
+        for (Venda item : carrinho) {
+            try {
+                processaVenda(item);
+                atualizaEstoque(item);
+                valorTotalVenda = calculaValorTotalVenda(valorTotalVenda, item);
+
+                System.out.println("\n" +  "Venda realizada com sucesso!" +
+                        "\nValor da venda: " + item.getValorVenda() +
+                        "\nProduto: " + item.getProduto().getDescricao());
+            }
+            catch (VendaException e) {
+                System.out.println(e.toString());
+                listaVendas.clear();
+                break;
+            }
         }
+        carrinho.clear();
+    }
+
+    public static void imprimeListaVendas() {
+        if(listaVendas == null) {
+            System.out.println("-------------------");
+            System.out.println("Produtos Comprados: ");
+            for (Venda venda : listaVendas) {
+                System.out.println(venda);
+                System.out.println("-------------------");
+            }
+            System.out.println("-------------------");
+        }
+        System.out.println("Não foi possível concluir a venda");
+    }
+    private static void processaVenda(Venda item) throws VendaException {
+        if (!checaEstoqueParaProcessarCompra(item.getProduto(), item.getQuantidade())) {
+            throw new VendaException("Erro no processamento da venda", "Não tem estoque suficiente", CodigoErro.PROCESSA_VENDA_ERRO);
+        }
+        item.setValorVenda(item.getProduto().getPreco().multiply(item.getQuantidade()));
+        item.setDataVenda(LocalDateTime.now());
+        listaVendas.add(item);
+    }
+
+    private static void atualizaEstoque(Venda item) {
+        item.getProduto().setEstoque(item.getProduto().getEstoque().subtract(item.getQuantidade()));
     }
 
     public static void adicionarCarrinhoCompras(Produto produto, BigDecimal quantidade) {
@@ -33,44 +67,12 @@ public class VendaService {
     }
 
 
-    private static void possivelConcluirCompra(Venda item, BigDecimal valorTotalVenda) throws VendaException {
-        if(!listaVendas.contains(item)) {
-            throw new VendaException("Erro na compra do produto",
-                    "O produto não tem estoque para a quantidade que deseja comprar",
-                    CodigoErro.COMPRA_NAO_CONCLUIDA);
-        }
-        else {
-            valorTotalVenda = valorTotalVenda.add(item.getValorVenda());
-        }
+    private static BigDecimal calculaValorTotalVenda(BigDecimal valorAtual, Venda item) {
+        return valorAtual.add(item.getValorVenda());
     }
 
-    public static void concluirComprar() {
-        BigDecimal valorTotalVenda = BigDecimal.ZERO;
-        for (Venda item : carrinho) {
-            try {
-                registraVenda(item);
-                possivelConcluirCompra(item, valorTotalVenda);
-
-            }
-            catch (VendaException e) {
-                System.out.println(e.toString());
-            }
-        }
-        System.out.println("Valor total: " + valorTotalVenda);
-        carrinho.clear();
-    }
-
-    private static boolean possivelComprar(Produto produto, BigDecimal quantidade) {
+    private static boolean checaEstoqueParaProcessarCompra(Produto produto, BigDecimal quantidade) {
         return produto.getEstoque() != null && produto.getEstoque().compareTo(quantidade) >= 0 && produto.getPreco() != null;
     }
 
-    public static void imprimeListaVendas() {
-        System.out.println("-------------------");
-        System.out.println("Produtos Comprados: ");
-        for (Venda venda : listaVendas) {
-            System.out.println(venda);
-            System.out.println("-------------------");
-        }
-        System.out.println("-------------------");
-    }
 }
